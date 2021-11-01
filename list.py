712a79a414d56a9f7579e4ea6044033c72e49d8b
priana <herian.sap@gmail.com>
2021-11-01 23:03:27 +0700

    list atau array pada python

---
+++ list.py
@@ -0,0 +1,14 @@
+nama = ["priana","sandi",'Hendra']
+
+#menambahkan data
+nama.append("Muldi")
+
+# menghapus data yang ada di list
+nama.remove("priana")
+# print
+
+#merubah data pada list
+nama[0] = "Kurniawan"
+
+print(nama)
+
