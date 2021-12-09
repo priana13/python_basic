616ff7d39b8356df2cb7ec718a7f3f42a51c5f50
priana13 <herian.sap@gmail.com>
2021-12-09 22:07:14 +0700

    Belajar tipe data tuple

---
+++ jaman-now/break.py
@@ -0,0 +1,9 @@
+# break berfungsi untuk men stop proses tertentu dalam looping
+
+for i in range(1,100):
+    if i % 10 == 0:
+        break    
+    print(i)
+
+#perulangan yang tidak bisa berhenti kecuali pake break
+#while True:
---
+++ jaman-now/tuple.py
@@ -0,0 +1,8 @@
+# tuple tidak bisa diubah datanya
+pelanggan = ("Priana","Adi","Andi","Muldi","Aziz")
+
+# jadi tidak bisa serpti ini:
+# pelanggan[0] = "Sukma", akan error
+
+print(pelanggan)
+print(pelanggan[0])
