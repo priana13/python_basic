5b6dc068ff3fe0f63c1731e9e1abe13279cf9f59
= <=>
2022-02-08 08:16:46 +0700

    Manggil Modul di Python

---
+++ jaman-now/function.py
@@ -0,0 +1,9 @@
+def say_hello(nama):
+    return f"Hello {nama}"
+
+def total( *list_angka):
+    hasil = 0
+    for data in list_angka:
+        hasil = hasil + data
+
+    return hasil
---
+++ jaman-now/module.py
@@ -0,0 +1,15 @@
+#belajar modul
+
+#import semua method
+# import function
+
+#import satu function saja
+from function import say_hello
+
+# cara menggunakan method yang di import semua method
+# hello = function.say_hello("Eko")
+
+# cara menggil method yang dimport satu method saja dari sebuah file
+hello = say_hello("Priana")
+
+print(hello)
