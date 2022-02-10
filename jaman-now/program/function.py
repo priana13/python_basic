452a285c69c058ad2706ff69b3422d169ef10796
= <=>
2022-02-10 11:26:24 +0700

    membuat daftar kontak

---
+++ jaman-now/program/function.py
@@ -0,0 +1,22 @@
+
+
+def display_kontak(daftar_kontak):
+    for kontak in daftar_kontak:
+        print("==================")
+        print(f"Nama : {kontak['nama']}")
+        print(f"Email : {kontak['email']}")
+        print(f"Telepon : {kontak['telepon']}")
+
+
+def new_kontak():
+    nama = input("Nama: ")
+    email = input("Email: ")
+    telepon = input("Telepon: ")
+    kontak = {
+        "nama" : nama,
+        "email" : email,
+        'telepon' : telepon
+    }
+
+    return kontak
+    
